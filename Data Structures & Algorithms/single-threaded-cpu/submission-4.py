2a2cef5a765b3e9d3e84245339422a024d592ddd
Qiaozhi_Bao <157238706+QiaozhiTo@users.noreply.github.com>
2026-06-05 09:30:24 -0400

    Add: single-threaded-cpu - submission-4

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i, t in enumerate(tasks):
+            t.append(i)
+        tasks.sort(key = lambda t:t[0])
+        minHeap, res = [], []
+        i = 0
+        time = tasks[0][0]
+
+        while minHeap or i < len(tasks):
+            while i < len(tasks) and time >= tasks[i][0]:
+                heapq.heappush(minHeap, [tasks[i][1], tasks[i][2]])
+                i += 1
+            
+            if not minHeap:
+                time = tasks[i][0]
+            else:
+                procTime, ind = heapq.heappop(minHeap)
+                time += procTime
+                res.append(ind)
+        return res
